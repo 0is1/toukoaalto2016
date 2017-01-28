b3b1ccde2917945dd0b6fcbd61fbb0f338755de9
Janne Saarela <janne.juhani@gmail.com>
2017-01-29 00:57:25 +0200

    Add also mobile menu

--- templates/header.php
+++ templates/header.php
@@ -1,19 +1,42 @@
 <?php
 use Roots\Sage\Setup;
+
 ?>
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-    <nav class="nav-primary">
-		<?php
-        if ( has_nav_menu( 'primary_navigation' ) ) :
-            wp_nav_menu( [
-                'theme_location' => 'primary_navigation',
-                'menu_class' => 'nav nav-pills js-main-menu',
-                'walker' => new Setup\Bootstrap_Walker(),
-            ] );
-        endif;
-		?>
-    </nav>
-  </div>
+<header class="main-header">
+    <div class="main-header__container js-header-container">
+        <div class="container">
+            <a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+            <nav class="nav-primary">
+                <div class="nav-primary__desktop">
+                    <?php
+                    if ( has_nav_menu( 'primary_navigation' ) ) :
+                        wp_nav_menu( [
+                            'theme_location' => 'primary_navigation',
+                            'menu_class' => 'nav nav-pills js-main-menu',
+                            'walker' => new Setup\Bootstrap_Walker(),
+                        ] );
+                    endif;
+                    ?>
+                </div>
+                <div class="nav-primary__mobile js-menu-container">
+                    <a class="nav-primary__mobile-toggle" href="#" data-action="toggle-main-menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </a>
+                    <?php
+                    if ( has_nav_menu( 'primary_navigation' ) ) :
+                        wp_nav_menu([
+                            'theme_location' => 'primary_navigation',
+                            'menu_class' => 'nav nav-pills js-main-menu',
+                            'container_class' => 'nav-primary__mobile-menu',
+                            'walker' => new Setup\Bootstrap_Walker(),
+                        ]);
+                    endif;
+                    ?>
+                </div>
+            </nav>
+        </div>
+    </div>
 </header>
