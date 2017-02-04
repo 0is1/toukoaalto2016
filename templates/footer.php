3a6b1e8daf9c22fe17f3348cd40dcd97e9868495
Janne Saarela <janne.juhani@gmail.com>
2017-02-04 19:19:48 +0200

    Add footer content

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,29 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar( 'sidebar-footer' ); ?>
+<footer class="site-footer">
+  <div class="container site-footer__wrapper">
+    <section class="site-footer__section">
+    <?php if ( $text_left = get_field( ACF_FOOTER_INFO_LEFT, ACF_OPTION_KEY ) ) : ?>
+        <h3><?php echo get_field( ACF_FOOTER_INFO_HEADER_LEFT, ACF_OPTION_KEY ); ?></h3>
+        <p class="site-footer__text-left"><?php echo $text_left; ?></p>
+    <?php endif; ?>
+    </section>
+    <section class="site-footer__section">
+    <?php if ( $text_right = get_field( ACF_FOOTER_INFO_RIGHT, ACF_OPTION_KEY ) ) : ?>
+        <h3><?php echo get_field( ACF_FOOTER_INFO_HEADER_RIGHT, ACF_OPTION_KEY ); ?></h3>
+        <p class="site-footer__text-right"><?php echo $text_right; ?></p>
+    <?php endif; ?>
+    </section>
+    <section class="site-footer__section site-footer__logo">
+    <?php if ( $image_id = get_field( ACF_FOOTER_LOGO, ACF_OPTION_KEY ) ) :
+        $img = wp_get_attachment_image_src( $image_id, 'medium' );
+        $img_src = $img[0];
+        ?>
+        <img src="<?php echo $img_src?>" />
+    <?php endif; ?>
+    </section>
+    <section class="site-footer__section">
+        <span class="created"><?php _e( 'Toteutus: ', THEME_SLUG );?>
+            <a href="<?php echo esc_url( 'https://jannejuhani.me' );?>" title="jannejuhani.me">jannejuhani</a>
+        </span>
+    </section>
   </div>
 </footer>
