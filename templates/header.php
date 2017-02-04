6c1e26685fd5813a1a2d13ef7acb5ee70b8d4720
Janne Saarela <janne.juhani@gmail.com>
2017-02-04 19:39:17 +0200

    Add search

--- templates/header.php
+++ templates/header.php
@@ -4,8 +4,8 @@ use Roots\Sage\Setup;
 ?>
 <header class="main-header">
     <div class="main-header__container js-header-container">
-        <div class="container">
-            <a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+        <div class="container-fluid">
+            <a class="brand" title="<?php echo get_site_url(); ?>" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
             <nav class="nav-primary">
                 <div class="nav-primary__desktop">
                     <?php
@@ -13,10 +13,14 @@ use Roots\Sage\Setup;
                         wp_nav_menu( [
                             'theme_location' => 'primary_navigation',
                             'menu_class' => 'nav nav-pills js-main-menu',
+                            'container_class' => 'nav-primary__desktop-menu',
                             'walker' => new Setup\Bootstrap_Walker(),
                         ] );
                     endif;
                     ?>
+                    <span class="nav-primary__search">
+                        <?php get_search_form(); ?>
+                    </span>
                 </div>
                 <div class="nav-primary__mobile js-menu-container">
                     <a class="nav-primary__mobile-toggle" href="#" data-action="toggle-main-menu">
@@ -35,6 +39,9 @@ use Roots\Sage\Setup;
                         ]);
                     endif;
                     ?>
+                    <span class="nav-primary__search">
+                        <?php get_search_form(); ?>
+                    </span>
                 </div>
             </nav>
         </div>
