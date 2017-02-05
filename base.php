713ce6204e2a9e6e61b343a97e603a64620efe1e
Janne Saarela <janne.juhani@gmail.com>
2017-02-05 10:54:25 +0200

    Add browsehappy class

--- base.php
+++ base.php
@@ -10,7 +10,7 @@ use Roots\Sage\Wrapper;
 	<?php get_template_part( 'templates/head' ); ?>
   <body <?php body_class(); ?>>
     <!--[if IE]>
-      <div class="alert alert-warning">
+      <div class="alert alert-warning browsehappy">
         <?php _e( 'You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', THEME_SLUG ); ?>
       </div>
     <![endif]-->
