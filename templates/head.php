b6a1fccc67482e8886f4a0c93c9d8324cd2a3204
Janne Saarela <janne.juhani@gmail.com>
2017-02-12 17:10:28 +0200

    Change font

--- templates/head.php
+++ templates/head.php
@@ -1,8 +1,7 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,400i,700,900" rel="stylesheet">
- 
-  <?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Exo:300,400,400i,700,900" rel="stylesheet">
+    <?php wp_head(); ?>
 </head>
