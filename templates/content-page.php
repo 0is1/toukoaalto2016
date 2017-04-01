d4bed3182f32a1fe01ef78e8765517152f8fa543
Janne Saarela <janne.juhani@gmail.com>
2017-04-01 10:46:42 +0300

    Show image on page if set

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,7 @@
-<?php the_content(); ?>
+<div class="entry-content">
+    <?php if ( has_post_thumbnail() ) : ?>
+        <?php the_post_thumbnail( IMAGE_SIZE_WIDE_LG ); ?>
+    <?php endif; ?>
+    <?php the_content(); ?>
+</div>
 <?php wp_link_pages( [ 'before' => '<nav class="page-nav"><p>' . __( 'Pages:', THEME_SLUG ), 'after' => '</p></nav>' ] ); ?>
